4d0200ab7b3ff559c5b45712aef6f0fddfd33de6
predohr <pedro.henrique9760@icloud.com>
2024-03-22 22:42:59 -0300

    Added class questao,

--- atividade1/lib/main.dart
+++ atividade1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -26,7 +27,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
             ),
             body: Column(
               children: [
-                Text(perguntas[_perguntaSelecionada]),
+                Questao(perguntas[_perguntaSelecionada]),
                 ElevatedButton(
                   child: Text('resposta 1'),
                   onPressed: _responder,
