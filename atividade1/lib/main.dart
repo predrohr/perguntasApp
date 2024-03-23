3cdc548f9fb2356ca459ef11f5b12cf7662af22e
predohr <pedro.henrique9760@icloud.com>
2024-03-22 22:28:05 -0300

    added private members.

--- atividade1/lib/main.dart
+++ atividade1/lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-class PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
 
-  void responder() {
+  void _responder() {
     setState(() {
-      perguntaSelecionada++;
+      _perguntaSelecionada++;
     });
-    print(perguntaSelecionada);
+    print(_perguntaSelecionada);
   }
 
   @override
@@ -26,18 +26,18 @@ class PerguntaAppState extends State<PerguntaApp> {
             ),
             body: Column(
               children: [
-                Text(perguntas[perguntaSelecionada]),
+                Text(perguntas[_perguntaSelecionada]),
                 ElevatedButton(
                   child: Text('resposta 1'),
-                  onPressed: responder,
+                  onPressed: _responder,
                 ),
                 ElevatedButton(
                   child: Text('resposta 2'),
-                  onPressed: responder,
+                  onPressed: _responder,
                 ),
                 ElevatedButton(
                   child: Text('resposta 3'),
-                  onPressed: responder,
+                  onPressed: _responder,
                 ),
               ],
             )));
@@ -46,7 +46,7 @@ class PerguntaAppState extends State<PerguntaApp> {
 
 class PerguntaApp extends StatefulWidget {
   @override
-  PerguntaAppState createState() {
-    return PerguntaAppState();
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
